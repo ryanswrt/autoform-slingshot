55e7ecb47da9a36f26dc880fd39dab28ef4e1466
Tim Brandin <tim.brandin@gmail.com>
2015-08-04 10:33:20 +0200

    Version bump

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "timbrandin:autoform-slingshot",
   summary: "File upload for AutoForm with Slingshot",
   description: "File upload for AutoForm with Slingshot",
-  version: "1.0.0",
+  version: "1.1.0",
   git: "http://github.com/timbrandin/autoform-slingshot.git"
 });
 
